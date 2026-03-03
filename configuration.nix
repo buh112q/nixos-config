f3e08a8665b6543f1a892bb7f0ded0db0d87fdab
buh112q <110540141+buh112q@users.noreply.github.com>
2026-03-03 14:07:06 +0700

    Add files via upload

--- configuration.nix
+++ configuration.nix
@@ -2,55 +2,30 @@
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [ 
       ./hardware-configuration.nix
     ];
 
-  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages; ## LTS
+  ###linuxPackges_latest
 
-  # Use latest kernel. ###linuxPackges_latest
-  boot.kernelPackages = pkgs.linuxPackages;
 
-  networking.hostName = "NixOS"; # Define your hostname.
+  networking.hostName = "NixOS";
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Enable networking
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "Asia/Bangkok";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
-
 
   fonts.packages = with pkgs; [
 	nerd-fonts.jetbrains-mono
 	font-awesome	
   ];
   
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  # services.xserver.enable = true;
   services.displayManager.ly.enable = true;
-  programs.niri = {
-  	enable = true;
-  	# useNautilus = true;
-  };
+  programs.niri.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -58,16 +33,12 @@
     variant = "";
   };
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
   hardware.graphics = {
   	enable = true;
   	enable32Bit = true;
   };
   
   hardware.bluetooth.enable = true;
-  # Enable sound with pipewire.
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -82,50 +53,38 @@
     # no need to redefine it in your config for now)
     #media-session.enable = true;
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Allow unfree packages
+  
   nixpkgs.config.allowUnfree = true;
-
-
+  
   programs.steam.enable = true;
   programs.chromium.enable = true;
   
   programs.fish.enable = true;
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.aa = {
     isNormalUser = true;
     shell = pkgs.fish;
     description = "aa";
     extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      # kdePackages.kate
-    #  thunderbird
-    ];
+    packages = with pkgs; [];
   };
-
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
   	wl-clipboard
-	git
-	gh
-	localsend
+  	git
+  	gh
+  	localsend
   	chromium
-	nautilus
+  	nautilus
   	micro
   	alacritty
+  	kitty
+  	rofi
   	fuzzel
-    waybar
+  	waybar
   	xwayland-satellite	
   ];
-
-  # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
@@ -135,7 +94,6 @@
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
-
-  system.stateVersion = "25.11"; # Did you read the comment?
+  system.stateVersion = "25.11";
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 }
